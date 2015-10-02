d1e3a8e0d015cfc1f7bf223294c812e3ff30c7eb
Adam <adam.kinson@gmail.com>
2015-10-02 10:23:13 -0400

    finish basic setup

--- js/controllers/toDoCtrl.js
+++ js/controllers/toDoCtrl.js
@@ -2,9 +2,26 @@ toDo.controller('toDoCtrl',
   ['$scope',
   function($scope){
 
-    $scope.item = { text: "Get groceries from the store",
-                    dueDate: new Date(),
-                    completed: false };
+    $scope.items =  [{
+                      text: "Get groceries from the store",
+                      dueDate: new Date(),
+                      completed: false
+                    },
+                    {
+                      text: "Vacuum living room",
+                      dueDate: new Date(),
+                      completed: false
+                    },
+                    {
+                      text: "Cure hip displasia in dogs",
+                      dueDate: new Date(),
+                      completed: false
+                    },
+                    {
+                      text: "Pay rent",
+                      dueDate: new Date(),
+                      completed: false
+                    }];
 
 
 
