c07bec165521ce288a70d37b4d543907cca11db7
Adam <adam.kinson@gmail.com>
2015-10-02 12:59:55 -0400

    refactor CRUD into service

--- js/controllers/toDoCtrl.js
+++ js/controllers/toDoCtrl.js
@@ -1,6 +1,6 @@
 toDo.controller('toDoCtrl',
-  ['$scope', '$window',
-  function($scope, $window){
+  ['$scope', '$window', 'toDoService',
+  function($scope, $window, toDoService){
 
     $scope.completedFilter = {
       active: false,
@@ -14,60 +14,25 @@ toDo.controller('toDoCtrl',
     };
 
 
-    $scope.items =  [{
-                      text: "Get groceries from the store",
-                      dueDate: new Date(),
-                      completed: false
-                    },
-                    {
-                      text: "Vacuum living room",
-                      dueDate: new Date(),
-                      completed: false
-                    },
-                    {
-                      text: "Cure hip displasia in dogs",
-                      dueDate: new Date(),
-                      completed: false
-                    },
-                    {
-                      text: "Pay rent",
-                      dueDate: new Date(),
-                      completed: false
-                    }];
+    $scope.items =  toDoService.getItems();
+
 
     $scope.submitToDo = function() {
       if ($scope.newForm.$valid) {
-        $scope.items.push($scope.newToDo);
+        toDoService.saveNew($scope.newToDo);
         $scope.newToDo = {};
         $scope.newForm.$setPristine();
-      };
+      }
     };
 
 
     $scope.destroy = function(item) {
-      var index = $scope.items.indexOf(item);
-      $scope.items.splice(index, 1);
+      toDoService.destroy(item);
     };
 
 
     $scope.clearCompleted = function() {
-      // sort Completed to front;
-      $scope.items.sort(function(a, b) { return b.completed } );
-
-      var numberCompleted = $scope.countCompleted();
-      $scope.items.splice(0, numberCompleted);
-    }
-
-
-    $scope.countCompleted = function() {
-      var count = 0;
-      for(i=0; i<$scope.items.length; i++) {
-        if (!$scope.items[i].completed) {
-          count = i;
-          break;
-        };
-      };
-      return count;
+      toDoService.destroyCompleted();
     };
 
 
---
+++ js/services/toDoService.js
@@ -0,0 +1,65 @@
+toDo.factory('toDoService', function() {
+
+  var obj = {};
+
+  obj._items =  [{
+                  text: "Get groceries from the store",
+                  dueDate: new Date(),
+                  completed: false
+                },
+                {
+                  text: "Vacuum living room",
+                  dueDate: new Date(),
+                  completed: false
+                },
+                {
+                  text: "Cure hip displasia in dogs",
+                  dueDate: new Date(),
+                  completed: false
+                },
+                {
+                  text: "Pay rent",
+                  dueDate: new Date(),
+                  completed: false
+                }];
+
+  obj.getItems = function() { return this._items };
+
+
+  obj.saveNew = function(newToDo) {
+    this._items.push(newToDo);
+  };
+
+
+  obj.destroy = function(item) {
+    var index = this._items.indexOf(item);
+    this._items.splice(index, 1);
+  };
+
+
+
+  obj.destroyCompleted = function() {
+    // sort Completed to front;
+    this._items.sort(function(a, b) { return b.completed } );
+
+    var numberCompleted = this._countCompleted();
+    this._items.splice(0, numberCompleted);
+  }
+
+
+  obj._countCompleted = function() {
+    var count = 0;
+    for(i=0; i<this._items.length; i++) {
+      if (!this._items[i].completed) {
+        count = i;
+        break;
+      };
+    };
+    return count;
+  };
+
+
+
+
+  return obj;
+});
